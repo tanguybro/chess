33159fa2d67960bb3c12c216af3c66c2e8523cda
tanguybro <tanguy.brouassin@gmail.com>
2022-01-15 18:49:14 +0100

    add positions

--- src/app/chessboard/chessboard.component.ts
+++ src/app/chessboard/chessboard.component.ts
@@ -11,8 +11,10 @@ export class ChessboardComponent implements OnInit {
 
   pieces = PIECES;
   selectedPiece?: Piece;
-  boardSize?: number;
-  cellSize?: number;
+  boardSize?: number = 0;
+  cellSize: number = 0;
+  cells_x = new Map<string, number>();
+  cells_y = new Map<string, number>();
 
   constructor() { 
 
@@ -20,22 +22,43 @@ export class ChessboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.boardSize = $('#chessboard').width();
-    if(this.boardSize != null) {
-      this.cellSize =  this.boardSize / 8;
+    if(this.boardSize == null) {
+      this.boardSize = 0;
     }
+    this.cellSize =  this.boardSize / 8;
+    this.initCellsPosition();
   }
 
   ngAfterViewInit(): void {
     // Init pieces position
-    var boardPosition = $('#chessboard').offset();
-    alert(boardPosition?.top)
     for(let piece of this.pieces) {
-      $('#' + piece.id).css({"position": "absolute", "top": boardPosition?.top + "px", "left": boardPosition?.left + "px"});
+      $('#' + piece.id).css({"position": "absolute", "top": this.cells_y.get(piece.position) + "px", "left": this.cells_x.get(piece.position) + "px"});
     }
   }
 
   onSelect(piece: Piece): void {
     this.selectedPiece = piece;
-    $('#' + piece.id).css({"position": "absolute", "top": "20px"});
+    $('.piece').removeClass('border border-primary')
+    $('#' + piece.id).addClass('border border-primary')
+    //$('#' + piece.id).css({"position": "absolute", "top": "20px"});
+  }
+
+  onClick(): void {
+    $('.piece').removeClass('border border-primary')
+  }
+
+  initCellsPosition(): void {
+    let columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+    if(this.boardSize == null) return;
+    
+    for(let row = 1; row <= 8; row++) {
+        for (let column of columns) {
+            let x = this.boardSize + this.cellSize * (column.charCodeAt(0) - 97) - 10;
+            let y = this.cellSize * (row + 1) - 10;
+            this.cells_x.set(column + row, x);
+            this.cells_y.set(column + row, y);
+      }
+    }
   }
 }
